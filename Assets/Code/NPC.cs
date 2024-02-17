1ec93627eaa6541d12b93c82cf3ccb9936807a3f
PH03N1XGithub <84901137+PH03N1XGithub@users.noreply.github.com>
2024-02-17 07:04:43 +0300

    commit 8

--- Assets/Code/NPC.cs
+++ Assets/Code/NPC.cs
@@ -2,14 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
+using Unity.Mathematics;
+using System;
 
 public class NPC : MonoBehaviour
 {
-    public NPCConversation myConversation;
+    public NPCConversation[] myConversation;
+    public int myConversationCount;
+    public float Distace;
+    public float range = 5;
+    public Transform target;
 
-    private void OnMouseOver()
+    public GameObject UI;
+
+    private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-            ConversationManager.Instance.StartConversation(myConversation);
+        Distace = gameObject.transform.position.x -target.position.x;
+        Distace = Mathf.Abs(Distace);
+        if (Distace < range)
+        {
+            UI.SetActive(true);
+        }
+        else
+        {
+            UI.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ConversationManager.Instance.SetInt("New1", 2);
+            print(ConversationManager.Instance.GetInt("New1"));
+        }
+
     }
+
+    public void OnMouseOver()
+    {
+        if (Distace > range)
+            return;
+        if(Input.GetMouseButtonDown(0))
+            ConversationManager.Instance.StartConversation(myConversation[myConversationCount]);
+    }
+
+
+
 }
