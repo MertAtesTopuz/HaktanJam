461547c6ad40e2241e87fa47ecf302016d265954
Mert Ateş Topuz <m.atestopuz@gmail.com>
2024-02-18 07:06:58 +0300

    Commit 18
    Menü düzenlendi, kedi tuşu ve quit tuşu eklendi

--- Assets/Code/MenSicrip.cs
+++ Assets/Code/MenSicrip.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenSicrip : MonoBehaviour
 {
     public Button button;
+    public GameObject quitTxt;
+
+    private AudioSource audioSrc;
+    private bool audioControl = true;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
 
     public void load()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitBtn()
+    {
+        StartCoroutine(QuitTxtTimer());
+    }
+
+    public void CatBtn()
+    {
+        if(audioControl == true)
+        {
+            StartCoroutine(CatAudio());
+        }
+    }
+
+    IEnumerator QuitTxtTimer()
+    {
+        quitTxt.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        quitTxt.SetActive(false);
+    }
+
+    IEnumerator CatAudio()
+    {
+        audioSrc.Play();
+        audioControl = false;
+        yield return new WaitForSeconds(2f);
+        audioSrc.Stop();
+        audioControl = true;
     }
 }
